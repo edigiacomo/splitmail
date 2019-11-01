4283749dcc2cc5741ed8be549636448b6d2f3cd9
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2019-11-01 07:31:57 +0100

    version 0.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='splitmailbox',
-    version='0.0.0',
+    version='0.1',
     description='Simple tool to split your mailbox',
     license='GPLv2+',
     author='Emanuele Di Giacomo',
-    author_email='emanuele.digiacomo@gmail.com',
+    author_email='emanuele@digiacomo.cc',
     url='https://github.com/edigiacomo/splitmailbox',
     packages=['splitmailbox'],
     entry_points={
